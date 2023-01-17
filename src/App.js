a0c0809ac0162f95b93c05f7c98c4d437fb03801
Agustinja21 <110314558+Agustinja21@users.noreply.github.com>
2023-01-17 14:29:46 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header/Header";
+import Homepage from "./components/Homepage/Homepage";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Notfound from "./components/Notfound/Notfound";
+import FoodDetails from "./components/FoodDetails/FoodDetails";
+import CartReview from "./components/CartReview/CartReview";
+//
+import { toast } from "react-toastify";
+import Login from "./components/Auth/Login/Login";
+import Register from "./components/Auth/Register/Register";
 
+toast.configure({
+  autoClose: 1000,
+  draggable: false,
+});
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/review/cart" component={CartReview} />
+          <Route exact path="/food/details/:keys" component={FoodDetails} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <Route path="*" component={Notfound} />
+        </Switch>
+      </Router>
     </div>
   );
 }
